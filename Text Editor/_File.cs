255aa83e206270a2c466e4da5e472afaa591fcfb
Dağhan Güven <moderator0180@gmail.com>
2024-02-15 18:11:58 +0300

    Add files via upload

---
+++ Text Editor/_File.cs
@@ -0,0 +1,13 @@
+﻿namespace Text_Editor
+{
+    class _File
+    {
+        public _File()
+        {
+            Path = "Unknown";
+            Saved = false;
+        }
+        public string Path;
+        public bool Saved;
+    }
+}
